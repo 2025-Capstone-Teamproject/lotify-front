449b9705af6031f6b40a1281906ef9e3b353892a
0kent0 <1002wjdgns@naver.com>
2025-05-23 11:25:45 +0900

    0523

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -8,7 +9,12 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Main Page"),
+        child: ElevatedButton(
+          onPressed: () {
+            context.go('/vehicle'); // GoRouter 사용
+          },
+          child: Text("차량 등록"),
+        ),
       )
     );
   }
